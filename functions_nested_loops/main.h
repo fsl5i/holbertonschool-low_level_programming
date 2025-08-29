4343c0c3362ae3952afc09d1c5d32123e18e7880
fsl5i <busbar51.com@gmail.com>
2025-08-29 18:41:52 +0000

    updates

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,14 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+void print_times_table(int n);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
 int _putchar(char c);
 void print_alphabet(void);
+int _isalpha(int c);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void);
-
-#endif
