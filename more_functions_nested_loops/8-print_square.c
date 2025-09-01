83562ff32b5d86663b7e929554577c9141ff3a7a
root <root@ip-10-42-218-212.ec2.internal>
2025-09-01 11:05:46 +0000

    Fix Betty style in 8-print_square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,18 +8,21 @@
  */
 void print_square(int size)
 {
-int r, c;
+    int r, c;
 
-if (size <= 0)
-{
-_putchar('\n');
-return;
-}
-    
-for (r = 0; r < size; r++)
-{
-for (c = 0; c < size; c++)
-_putchar('#');
-_putchar('\n');
-}
-}
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        for (r = 0; r < size; r++)
+        {
+            for (c = 0; c < size; c++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+}   
