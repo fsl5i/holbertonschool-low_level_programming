68bf3e3a71c919c22180dc928b7249437fa4c1a9
root <root@ip-10-42-64-230.ec2.internal>
2025-09-04 15:46:37 +0000

    Fix _atoi function and Betty style compliance

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,22 @@
  */
 int _atoi(char *s)
 {
-int i = 0;
-int sign = 1;
-int result = 0;
+    int i = 0;
+    int sign = 1;
+    unsigned int result = 0;
 
-while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-{
-if (s[i] == '-')
-sign *= -1;
-i++;
-}
+    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-while (s[i] >= '0' && s[i] <= '9')
-{
-result = result * 10 + (s[i] - '0');
-i++;
-}
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
 
-return (sign * result);
+    return (sign * result);
 }
