0644ef4c633c6c4faece0d4ef0d348e884dae259
root <root@ip-10-42-63-219.ec2.internal>
2025-09-05 16:42:41 +0000

    Add _strncpy function (task 2)

--- more_pointers_arrays_strings/2-strncpy.c
+++ more_pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
 /**
- * _strncpy - copies at most n bytes from src to dest
- * @dest: destination buffer
- * @src: source string
- * @n: number of bytes to copy
- *
- * Return: pointer to dest
- */
+  * _strncpy - Copy a string
+  * @dest: The destination value
+  * @src: The source value
+  * @n: The copy limit
+  *
+  * Return: char value
+  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
+	int a = 0, b = 0;
 
-while (i < n && src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
-}
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
-return (dest);
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
 }
