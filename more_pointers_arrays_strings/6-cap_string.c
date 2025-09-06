96a0f7d05abdf6de2db92ccd3f2f7f9762755e8b
root <root@ip-10-42-22-184.ec2.internal>
2025-09-06 11:00:37 +0000

    Fix logic in 6-cap_string

--- more_pointers_arrays_strings/6-cap_string.c
+++ more_pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-  * cap_string - ...
-  * @s: ...
-  *
-  * Return: char value
-  */
+ * cap_string - Capitalizes all words of a string.
+ * @s: The string to modify
+ *
+ * Return: The modified string
+ */
 char *cap_string(char *s)
 {
-	int a = 0, i;
-	int cspc = 13;
-	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+    int a = 0, i;
+    char spc[] = {32, '\t', '\n', 44, ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-	while (s[a])
-	{
-		i = 0;
+    while (s[a])
+    {
+        if (a == 0 && s[a] >= 'a' && s[a] <= 'z')
+        {
+            s[a] -= 32;
+        }
 
-		while (i < cspc)
-		{
-			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] -= 32;
+        for (i = 0; i < 13; i++)
+        {
+            if (s[a] == spc[i] && s[a + 1] >= 'a' && s[a + 1] <= 'z')
+            {
+                s[a + 1] -= 32;
+            }
+        }
+        a++;
+    }
 
-			i++;
-		}
-
-		a++;
-	}
-
-	return (s);
+    return (s);
 }
