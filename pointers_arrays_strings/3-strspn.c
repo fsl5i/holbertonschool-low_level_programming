866a8ee0e95605dad53c1bfc0882a04fe33bba5d
root <root@ip-10-42-79-93.ec2.internal>
2025-09-11 12:49:49 +0000

    Implement _strspn function

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,48 +1,30 @@
 #include "main.h"
 
 /**
-  * _strcmp - Compares two strings
-  * @s1: The first string
-  * @s2: The second string
-  *
-  * Return: int value
-  */
-int _strcmp(char *s1, char *s2)
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to search in
+ * @accept: the string containing the characters to match
+ *
+ * Return: the number of bytes in the initial segment of s
+ *         consisting only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
 {
-	int a = 0, b = 0, c = 0, r = 0, lim;
+unsigned int i, j, count;
 
-	while (s1[a])
-	{
-		a++;
-	}
-
-	while (s2[b])
-	{
-		b++;
-	}
-
-	if (a <= b)
-	{
-		lim = a;
-	}
-	else
-	{
-		lim = b;
-	}
-
-	while (c <= lim)
-	{
-		if (s1[c] == s2[c])
-		{
-			c++;
-			continue;
-		}
-		else
-		{
-			r = s1[c] - s2[c];
-			break;
-		}
-	}
-
-	return (r);
+count = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+count++;
+break;
+}
+}
+if (accept[j] == '\0')
+return (count);
+}
+return (count);
 }
