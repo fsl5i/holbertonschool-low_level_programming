f1b1c108c68b25b83ba091a34522dfb22173697f
root <root@ip-10-42-208-225.ec2.internal>
2025-09-20 18:15:58 +0000

    Add/update malloc_free test files

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -4,5 +4,7 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+echo "int **alloc_grid(int width, int height);" >> main.h
+echo "void free_grid(int **grid, int height);" >> main.h
 
 #endif
