89e4a8d04114cfdfa3b2d8a132198625815998b7
root <root@ip-10-42-181-126.ec2.internal>
2025-09-27 18:07:31 +0000

    Implement print_strings function (variadic functions project)

---
+++ variadic_functions/2-main.c
@@ -0,0 +1,14 @@
+#include <stddef.h>          /* علشان يعرف NULL */
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    print_strings("; ", 4, "Hello", NULL, "World", "C");
+    return (0);
+}
