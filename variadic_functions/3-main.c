78224ec7cb38f992062c2d2edd6dd4564a122bd9
root <root@ip-10-42-181-126.ec2.internal>
2025-09-27 19:08:39 +0000

    Implement print_all function (variadic functions project)

---
+++ variadic_functions/3-main.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_all("ceis", 'B', 3, "stSchool");
+print_all("sfc", "Hello", 3.14, 'X');
+print_all("sifs", "Test", 42, 2.5, NULL);
+return (0);
+}
