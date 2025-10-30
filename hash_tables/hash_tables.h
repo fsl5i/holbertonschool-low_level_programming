a481d04310dae0a3577f624b5cdac99acd7d1b87
root <root@ip-10-42-31-25.ec2.internal>
2025-10-30 10:40:15 +0000

    fix fiel hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,35 +1,22 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include <stddef.h>
 
-/**
-	* struct hash_node_s - node of a hash table
-	* @key: the key, string, unique in the hash table
-	* @value: the value corresponding to a key
-	* @next: pointer to the next node (for collision handling)
-	*/
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
-/**
-	* struct hash_table_s - hash table data structure
-	* @size: the size of the array
-	* @array: an array of size @size, each cell is a pointer to the first node
-	*/
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif /* HASH_TABLES_H */
