62b8820c200ad1a145601f9b8f56118c20356ac9
root <root@ip-10-42-31-25.ec2.internal>
2025-10-30 09:59:31 +0000

    fix

--- hash_tables/2-main.c
+++ hash_tables/2-main.c
@@ -4,23 +4,23 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
- */
+	* main - check the code for Holberton School students.
+	*
+	* Return: Always EXIT_SUCCESS.
+	*/
 int main(void)
 {
-    char *s;
-    unsigned long int hash_table_array_size = 1024;
+	char *s;
+	unsigned long int hash_table_array_size = 1024;
 
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    return (EXIT_SUCCESS);
+	s = "cisfun";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+	s = "Don't forget to tweet today";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+	s = "98";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+	return (EXIT_SUCCESS);
 }
